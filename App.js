f167798f0385d107d4c9c1d4953a69ad5620381d
mathias SIMIYU <mathiassimiyu52@gmail.com>
2025-11-04 16:10:34 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,34 @@
+// App.js
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { LogBox } from "react-native";
+
+// Ignore all Firebase timer warnings
+LogBox.ignoreLogs(["Setting a timer"]);
+
+import { auth } from "./firebase"; // ✅ Firebase connection
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
