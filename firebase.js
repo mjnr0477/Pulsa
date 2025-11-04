ae833720fa4423cee4c3f47ef57b1f2b43416698
mathias SIMIYU <mathiassimiyu52@gmail.com>
2025-11-04 16:08:46 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBku2qgN1ftioVdfNcvnDgCWSkN9mwx0iA",
+  authDomain: "pulse-f5547.firebaseapp.com",
+  projectId: "pulse-f5547",
+  storageBucket: "pulse-f5547.firebasestorage.app",
+  messagingSenderId: "748217789233",
+  appId: "1:748217789233:android:245256b646a47bcb129e10",
+  measurementId: "G-XXXXXXXXXX" // optional, you can remove if not used
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
